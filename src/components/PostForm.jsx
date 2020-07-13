c40d76355e996035dacc5f65bc8dc580f45dec0c
Eugene Tsvetkov <orion.octupus@gmail.com>
2020-07-14 01:43:41 +0300

    add input field logic

--- src/components/PostForm.jsx
+++ src/components/PostForm.jsx
@@ -4,20 +4,50 @@ export default class PostForm extends React.Component {
     constructor(props) {
         super(props)
 
-        this.state = {}
+        this.state = {
+            title: ''
+        }
     }
 
-    submitHandler = (e) => {
-        e.preventDefault();
-        alert('huyk!')
+    submitHandler = (event) => {
+        event.preventDefault();
+        // console.log(e.target);
+        // alert(this.state.title);
+
+        const { title } = this.state;
+
+        const newPost = {
+            title, id: Date.now().toString()
+        }
+        console.log(newPost);
+        this.setState({ title: '' })
+    }
+
+    changeHandle = (event) => {
+        event.persist()
+        console.log(event.target.value);
+
+        // this.setState({
+        //     title: e.target.value
+        // })
+
+        //запись с использованием ES6 синтаксиса, коротко, красиво и универсально, но сложночитаемо
+        this.setState(prev => ({ ...prev, ...{ [event.target.name]: event.target.value } }))
     }
 
     render() {
         return (
             <form onSubmit={this.submitHandler}>
                 <div className="form-group">
-                    <label for="textInput">Заголовок поста</label>
-                    <input type="text" class="form-control" id="textInput" />
+                    <label htmlFor="textInput">Заголовок поста</label>
+                    <input
+                        type="text"
+                        className="form-control"
+                        id="textInput"
+                        name='title'
+                        value={this.state.title}
+                        onChange={this.changeHandle}
+                    />
                 </div>
                 <button className="btn btn-success" type="submit">Создать</button>
             </form>
